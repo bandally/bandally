4fe067f1766f3622ad9d11e4fc4f113da2e36afb
Yosuke Tanaka <yosket87@gmail.com>
2016-04-07 20:20:30 +0900

    gulpfileをec6で書く

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,14 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
-var del = require('del');
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import bower from 'bower';
+import concat from 'gulp-concat';
+import sass from 'gulp-sass';
+import minifyCss from 'gulp-minify-css';
+import rename from 'gulp-rename';
+import sh from 'shelljs';
+import del from 'del';
 
-var paths = {
+const paths = {
   html: ['src/*.html', 'src/**/*.html'],
   css : ['src/content/scss/*.scss'],
   img : ['src/content/img/*.{png,jpg,gif}'],
@@ -18,43 +18,38 @@ var paths = {
 
 gulp.task('default', ['html', 'css', 'img', 'js', 'lib', 'watch']);
 
-gulp.task('clean', function (done) {
-  // del(['www/**', '!www'], done);
-  done();
-});
-
-gulp.task('html', ['clean'], function (done) {
+gulp.task('html', done => {
   gulp.src(paths.html)
     .pipe(gulp.dest('www/'))
     .on('end', done);
 });
 
-gulp.task('css', ['clean'], function (done) {
+gulp.task('css', done => {
   gulp.src(paths.css)
     .pipe(sass())
     .pipe(gulp.dest('www/content/css/'))
     .on('end', done);
 });
 
-gulp.task('js', ['clean'], function (done) {
+gulp.task('js', done => {
   gulp.src(paths.js)
     .pipe(gulp.dest('www/app/'))
     .on('end', done);
 });
 
-gulp.task('img', ['clean'], function (done) {
+gulp.task('img', done => {
   gulp.src(paths.img)
     .pipe(gulp.dest('www/content/img/'))
     .on('end', done);
 });
 
-gulp.task('lib', ['clean'], function (done) {
+gulp.task('lib', done => {
   gulp.src(paths.lib)
     .pipe(gulp.dest('www/lib/'))
     .on('end', done);
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   gulp.watch(paths.html, ['html']);
   gulp.watch(paths.css , ['css']);
   gulp.watch(paths.img , ['img']);
@@ -62,14 +57,14 @@ gulp.task('watch', function () {
   gulp.watch(paths.lib , ['lib']);
 });
 
-gulp.task('install', ['git-check'], function() {
+gulp.task('install', ['git-check'], () => {
   return bower.commands.install()
-    .on('log', function(data) {
+    .on('log', data => {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
 });
 
-gulp.task('git-check', function(done) {
+gulp.task('git-check', done => {
   if (!sh.which('git')) {
     console.log(
       '  ' + gutil.colors.red('Git is not installed.'),
